101f6d66cf757fc20c499f6725a8bc7ed13ce206
Your Name <youremail@domain.com>
2017-07-17 15:30:58 +0300

    first commit

---
+++ db/migrate/20170717121655_add_due_date_to_tasks.rb
@@ -0,0 +1,4 @@
+class AddDueDateToTasks < ActiveRecord::Migration[5.1]
+  def change
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,19 @@
+ENV['RACK_ENV'] = 'test'
+require("rspec")
+  require("pg")
+  require("sinatra/activerecord")
+  require("task")
+  require("list")
+
+
+
+  RSpec.configure do |config|
+    config.after(:each) do
+      Task.all().each() do |task|
+      task.destroy()
+      List.all().each() do |list|
+        list.destroy()
+      end
+    end
+  end
+end
